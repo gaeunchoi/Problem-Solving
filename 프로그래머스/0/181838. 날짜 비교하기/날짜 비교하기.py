dc8b686c9ba092098484e4031e4f6f6955195b9c
Gaanii <56152020+gaeunchoi@users.noreply.github.com>
2025-04-01 17:05:52 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 9.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.py
@@ -0,0 +1,17 @@
+def solution(date1, date2):
+  y1, m1, d1 = date1
+  y2, m2, d2 = date2
+  if y1 < y2:
+    return 1
+  elif y1 > y2:
+    return 0
+  else:
+    if m1 < m2:
+      return 1
+    elif m1 > m2:
+      return 0
+    else:
+      if d1 < d2:
+        return 1
+      else:
+        return 0
