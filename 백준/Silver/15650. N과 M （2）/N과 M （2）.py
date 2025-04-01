6c118640a7429ddcf27937d90cb788f208338ea7
Gaanii <56152020+gaeunchoi@users.noreply.github.com>
2025-04-01 16:25:03 +0900

    [Silver III] Title: N과 M (2), Time: 72 ms, Memory: 30864 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,12 @@
+import sys
+import itertools
+
+N, M = map(int, sys.stdin.readline().split())
+
+# N을 1 ~ N까지 정수 배열로 바꿔주자
+N = [i+1 for i in range(N)]
+
+lists = list(itertools.combinations(N, M))
+
+for value in lists:
+    print(' '.join(map(str, value)))
