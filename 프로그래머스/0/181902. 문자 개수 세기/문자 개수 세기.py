982fdee868cd0fc32c242b20a5a5b6eaa3a20d7d
Gaanii <56152020+gaeunchoi@users.noreply.github.com>
2025-04-01 17:02:00 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.31 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -0,0 +1,8 @@
+def solution(my_string):
+    answer = [0] * 52
+    for str in my_string:
+        if str.isupper():
+          answer[ord(str)-65]+=1
+        else:
+           answer[ord(str)-71]+=1
+    return answer
