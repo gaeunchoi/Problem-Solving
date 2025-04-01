82e6a0311b2c2191f032dfcbaab3e4a439dad3fa
Gaanii <56152020+gaeunchoi@users.noreply.github.com>
2025-04-01 16:27:10 +0900

    [Silver III] Title: 소수 구하기, Time: 1012 ms, Memory: 127604 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,15 @@
+def primeNum(n):
+    if n < 2:
+        return False
+
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+
+    return True
+
+M, N = map(int, input().split())
+
+for i in range(M, N+1):
+    if primeNum(i) == True:
+        print(i)
