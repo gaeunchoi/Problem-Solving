f71066496df65e8957938e34cec94a217d7b8f1d
Gaanii <56152020+gaeunchoi@users.noreply.github.com>
2025-04-03 16:05:46 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -1,15 +1,9 @@
 def solution(cards1, cards2, goal):
-    result = []
-    idx1 = idx2 = 0
-    
-    for i in range(len(goal)):
-        if idx1 < len(cards1) and goal[i] == cards1[idx1]:
-            result.append(goal[i])
-            idx1 += 1
-        elif idx2 < len(cards2) and goal[i] == cards2[idx2]:
-            result.append(goal[i])
-            idx2 += 1
+    for g in goal:
+        if len(cards1) > 0 and g == cards1[0]:
+            cards1.pop(0)       
+        elif len(cards2) >0 and g == cards2[0]:
+            cards2.pop(0)
         else:
-            break
-
-    return "Yes" if goal == result else "No"
+            return "No"
+    return "Yes"
