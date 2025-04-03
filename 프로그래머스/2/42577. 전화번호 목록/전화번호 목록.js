fd37890663f9f6d009713809a3567c1005625d22
Gaanii <56152020+gaeunchoi@users.noreply.github.com>
2025-04-03 10:28:05 +0900

    [level 2] Title: 전화번호 목록, Time: 143.16 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,20 +1,7 @@
-function solution(phone_book) {
-    let phoneMap = new Map();
-    
-    for (let phone of phone_book) {
-        phoneMap.set(phone, true);
-    }
+function solution(phoneBook) {
+    return !phoneBook.sort().some((_,i)=> {
+        if(i === phoneBook.length -1) return false;
 
-    // 각 번호를 한 글자씩 줄여가면서 접두어 검사
-    for (let phone of phone_book) {
-        let prefix = "";
-        
-        for (let num of phone) {
-            prefix += num;
-            if (prefix !== phone && phoneMap.has(prefix)) {
-                return false;
-            }
-        }
-    }
-    return true; 
+        return phoneBook[i+1].startsWith(phoneBook[i]);        
+    })
 }
