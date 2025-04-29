aff945e41b746377cd3d63dd67c1a51f8750a707
Gaanii <56152020+gaeunchoi@users.noreply.github.com>
2025-04-29 14:48:03 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.42 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -0,0 +1,20 @@
+function solution(wallpaper) {
+    let lux = -1, luy = -1, rdx = -1, rdy = -1;
+    for(let i = 0 ; i < wallpaper.length ; i++){
+        for(let j = 0 ; j < wallpaper[0].length ; j++){
+            if(wallpaper[i][j] === "#"){
+                if(lux === -1) lux = i;       // lux 찾기
+                
+                if(luy === -1) luy = j;
+                else luy = Math.min(luy, j);  // luy 찾기
+                
+                rdx = i;                      // rdx 찾기 
+                
+                if(rdy === -1) rdy = j;
+                else rdy = Math.max(rdy, j);
+            } 
+            
+        }
+    }
+    return [lux, luy, rdx+1, rdy+1];
+}
