d8618afed5202867e1ed8923a215f8c378f32487
Gaanii <56152020+gaeunchoi@users.noreply.github.com>
2025-04-30 14:42:42 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.02 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -2,8 +2,8 @@ def solution(s):
     split_word = s.split(" ")
     result = []
     for sw in split_word:
-        if sw:  # 비어있지 않은 단어면 처리
+        if sw:
             result.append(sw[0].upper() + sw[1:].lower())
-        else:  # 빈 문자열 (공백)인 경우 그대로 유지
+        else:
             result.append("")
     return " ".join(result)
