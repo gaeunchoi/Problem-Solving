4eda001cdd92bc7d251c7805b72ee0d109c21151
Gaanii <56152020+gaeunchoi@users.noreply.github.com>
2025-05-12 14:05:56 +0900

    [level 2] Title: 방문 길이, Time: 0.40 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+def solution(dirs):
+    cx, cy = 0, 0
+    visited = set()
+    directions = {'U': [-1, 0], 'D': [1, 0], 'L': [0, -1], 'R': [0, 1]}
+
+    result = 0
+    for dir in dirs:
+        dx, dy = directions[dir]
+        mx, my = cx + dx, cy + dy
+        
+        if -5 <= mx <= 5 and -5 <= my <= 5:
+            visited.add(((cx, cy), (mx, my)) if (cx, cy) < (mx, my) else ((mx, my), (cx, cy)))
+            cx, cy = mx, my
+    return len(visited)
