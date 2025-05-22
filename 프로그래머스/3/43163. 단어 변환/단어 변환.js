af851706cda12ff48d190d37503169d16b21ec9f
Gaanii <56152020+gaeunchoi@users.noreply.github.com>
2025-05-22 13:35:26 +0900

    [level 3] Title: 단어 변환, Time: 0.43 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.js
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -1,29 +1,28 @@
-function solution(begin, target, words) {
-    const visited = {[begin]: 0};
-    const queue = [begin];
+function solution(begin, target, words){
+    let head = 0;
+    const q = [[begin, 0]];
+    const visited = new Set([begin]);
     
-    while(queue.length){
-        const cur = queue.shift();
-        if(cur === target) break;
+    const isOneLetterDiff = (a, b) => {
+        let cnt = 0;
+        for(let i = 0 ; i < a.length ; i++){
+            if(a[i] !== b[i]) cnt++;
+        }
+        return cnt === 1;
+    }
+    
+    while(head < q.length){
+        const [cur, step] = q[head++];
         
-        for(let w of words){
-            if(isOneLetterDiff(w, cur) && !visited[w]){
-                visited[w] = visited[cur] + 1;
-                queue.push(w);
+        if(cur === target) return step;
+        
+        for(const word of words){
+            if(isOneLetterDiff(cur, word) && !visited.has(word)){
+                visited.add(word);
+                q.push([word, step + 1]);
             }
         }
     }
     
-    return visited[target] ? visited[target] : 0;
-}
-
-const isOneLetterDiff = (a, b) => {
-    let cnt = 0;
-    const len = a.length;
-    
-    for(let i = 0 ; i < len ; i++){
-        if(a[i] !== b[i]) cnt++;
-    }
-    
-    return cnt === 1 ? true : false;
+    return 0;
 }
