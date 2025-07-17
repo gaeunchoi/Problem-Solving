844b9ee1991444bdcf8b82eaff9d597b4bd4f8a2
Gaanii <56152020+gaeunchoi@users.noreply.github.com>
2025-07-17 17:23:51 +0900

    [level 2] Title: 호텔 대실, Time: 6.74 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
@@ -1,24 +1,22 @@
-const convertTime = (t) => {
-    const [h, m] = t.split(":").map(Number);
-    return 60*h + m;
+const convertT = (t) => {
+    const [h, m] = t.split(":").map(Number)
+    return 60 * h + m
 }
 
 function solution(book_time) {
-    const sortedBookTime = book_time.map(([s, e]) => [convertTime(s), convertTime(e) + 10]).sort((a, b) => a[0] - b[0]);
-    
-    let rooms = [];
+    const sortedBookTime= book_time.map(([s, e]) => [convertT(s), convertT(e) + 10]).sort((a, b) => a[0] - b[0])
+       
+    let rooms = []
     for(const [s, e] of sortedBookTime){
-        let used = false;
-        for(let i = 0 ; i < rooms.length; i++){
+        let used = false
+        for(let i = 0 ; i < rooms.length ; i++){
             if(rooms[i] <= s){
-                rooms[i] = e;
-                used = true;
-                break;
+                rooms[i] = e
+                used = true
+                break
             }
         }
-        
-        if(!used) rooms.push(e);
+        if(!used) rooms.push(e)
     }
-    
-    return rooms.length;
+   return rooms.length
 }
